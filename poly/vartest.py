7831e34a5642862cb18b246d17cae67d607f60c6
Geuniv <kkoowl99@gmail.com>
2023-09-20 19:15:53 +0900

    2023.09.20 빅데이터프로그래밍기초

---
+++ poly/vartest.py
@@ -0,0 +1,8 @@
+# vartest.py
+
+a = 1
+def vartest(a):
+    a = a + 1
+
+vartest(3)
+print(a)
---
+++ poly/vartest_global.py
@@ -0,0 +1,10 @@
+# vartest_global.py
+
+a = 1
+
+def vartest():
+    global a
+    a = a + 1
+
+vartest()
+print(a)
