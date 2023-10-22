95078368eb7cfc121db3a45ea7aab771fd63fe04
Geuniv <kkoowl99@gmail.com>
2023-10-22 23:16:41 +0900

    2023.10.22 빅데이터프로그래밍기초

---
+++ poly/class4.py
@@ -0,0 +1,13 @@
+class Service:
+    secret = "영구는 배꼽이 두 개다."
+
+    def __init__(self, name):
+        self.name = name
+
+    def sum(self, a, b):
+        result = a + b
+        print("%s님 %s + %s = %s 입니다." % (self.name, a, b, result))
+
+
+pey = Service("최준상")
+pey.sum(1, 1)
