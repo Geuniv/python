8ed68be3a28fdb43c907f1d6c344ff1432f9fd4b
Geuniv <kkoowl99@gmail.com>
2024-03-07 23:48:39 +0900

    2024.03.07 백준

---
+++ BaekJoon/BJ_05_2908.py
@@ -0,0 +1,16 @@
+# if - else
+num1, num2 = input().split()
+num1 = int(num1[::-1])  # [::-1] : 역순
+num2 = int(num2[::-1])
+
+if num1 > num2:
+    print(num1)
+else :
+    print(num2)
+
+# 삼항연산자 코드
+num1, num2 = input().split()
+num1 = int(num1[::-1])  # [::-1] : 역순
+num2 = int(num2[::-1])
+
+print(num1) if num1 > num2 else print(num2)
