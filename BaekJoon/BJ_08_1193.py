3d3cd00c14bbdb07f6932701c1ff30850b9241c4
Geuniv <kkoowl99@gmail.com>
2024-08-16 23:26:24 +0900

    2024.08.16 ( 백준 예제 )

---
+++ BaekJoon/BJ_08_1193.py
@@ -0,0 +1,20 @@
+# 백준 단계별로 풀어보기 챕터 8 ( 일반 수학 1 )
+# 1193 ( 분수찾기 )
+
+num = int(input())
+line = 1
+
+while num > line:
+    num -= line
+    line += 1
+
+# 짝수일경우
+if line % 2 == 0:
+    a = num
+    b = line - num + 1
+# 홀수일경우
+elif line % 2 == 1:
+    a = line - num + 1
+    b = num
+
+print(f'{a}/{b}')
